bf6b0135c6487194a16cc882649cfde37421a078
David Gomes de Carvalho <david@dealersites.com.br>
2022-11-08 01:26:49 -0300

    first commit

---
+++ pages/index.js
@@ -0,0 +1,88 @@
+import config from "../config.json";
+import styled from "styled-components";
+import { CSSReset } from "../src/components/CSSReset";
+import Menu from "../src/components/Menu";
+import { StyledTimeline } from "../src/components/Timeline";
+
+
+function HomePage() {
+    const estilosHome = {
+        display: "flex",
+        flexDirection: "column",
+        flex: 1,
+    };
+    return (
+        <>
+            <CSSReset />
+            <div style={estilosHome}>
+                <Menu />
+                <Header></Header>
+                <Timeline playlists={config.playlists}></Timeline>
+            </div>
+        </>
+    );
+}
+  
+export default HomePage
+
+const StyledHeader = styled.div`
+    img {
+        width: 80px;
+        height: 80px;
+        border-radius: 50%;
+    }
+    .user-info {
+        display: flex;
+        align-items: center;
+        width: 100%;
+        padding: 16px 32px;
+        gap: 16px;
+        margin-top: 50px;
+    }
+`;
+function Header() {
+    return (
+        <StyledHeader>
+            <div className="user-info">
+                <img src={`https://github.com/${config.github}.png`}></img>
+                <div>
+                    <h2>
+                        {config.name}
+                    </h2>
+                    <p>
+                        {config.job}
+                    </p>
+                </div>
+            </div>
+        </StyledHeader>
+    )
+}
+function Timeline(propriedades) {
+    const playlistNames = Object.keys(propriedades.playlists);
+    return (
+        <StyledTimeline>
+            {playlistNames.map((playlistName) => {
+                const videos = propriedades.playlists[playlistName];
+                return (
+                    <section>
+                        <h2>{playlistName}</h2>
+                        <div>
+                            {
+                                videos.map((video => {
+                                    return (
+                                        <a href={video.url}>
+                                            <img src={video.thumb}></img>
+                                            <p>
+                                                {video.title}
+                                            </p>
+                                        </a>
+                                    )
+                                }))
+                            }
+                        </div>
+                    </section>
+                )
+            })}
+        </StyledTimeline>
+    )
+}
